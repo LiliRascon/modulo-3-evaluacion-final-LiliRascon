2ca5610022ec48ff3928d2985e9086bf06f34a6c
Lili Rascon <lilianarasconestrada@gmail.com>
2020-04-25 12:18:23 +0200

    add style to search

--- src/components/Filters.js
+++ src/components/Filters.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import '../stylesheets/Filter.scss';
 
 
 
@@ -16,18 +16,21 @@ const Filter = (props) => {
    }
 
    return (
-      <form onSubmit={preventDefault}>
+      <div>
+         <p className="text-search">INGRESA TU PERSONAJE FAVORITO</p>
+         <form onSubmit={preventDefault}>
+            {/* como los input los controlamos a traves de react por lo tanto lo hago a traves de las props */}
+            <input
+               name="title"
+               type="text"
+               placeholder="Ej. Rick"
+               value={props.value}
+               onChange={updateInputValue}
 
-         {/* como los input los controlamos a traves de react por lo tanto lo hago a traves de las props */}
-         <input
-            name="title"
-            type="text"
-            placeholder="Ej. Rick"
-            value={props.value}
-            onChange={updateInputValue}
-         />
+            />
 
-      </form>
+         </form>
+      </div>
    )
 }
 
